7bce5ac2f564d8ee46862db3157e99999aba015d
David Martin <drmartin1998@gmail.com>
2025-12-22 09:49:19 -0600

    bug: fixed build

--- lib/appointments.ts
+++ lib/appointments.ts
@@ -1,5 +1,5 @@
 import prisma from "./prisma";
-import { Prisma } from "./generated/prisma";
+import { Prisma } from "./generated/prisma/client";
 
 export interface AppointmentFilters {
   park?: string;
